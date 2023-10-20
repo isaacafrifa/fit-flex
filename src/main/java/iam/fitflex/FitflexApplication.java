5a000ea42b8a7c8c125f600016524fb17568b0bf
Afrifa <isaac.afrifa3@yahoo.com>
2023-10-21 02:45:34 +0300

    feat: create base for project

---
+++ src/main/java/iam/fitflex/FitflexApplication.java
@@ -0,0 +1,13 @@
+package iam.fitflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitflexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitflexApplication.class, args);
+    }
+
+}
