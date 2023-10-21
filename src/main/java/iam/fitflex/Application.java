bdbfa0005c9cb990ca797a3419c9bceefcc16d47
Afrifa <isaac.afrifa3@yahoo.com>
2023-10-21 03:06:24 +0300

    refactor: rename main class name

--- src/main/java/iam/fitflex/Application.java
+++ src/main/java/iam/fitflex/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FitflexApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(FitflexApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
